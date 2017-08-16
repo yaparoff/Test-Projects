b96f9b5ac85cf94ba948e13f6f3b8f052c3accba
Andrei_Iaparov <Andrei_Iaparov@epam.com>
2017-08-16 10:15:16 +0400

    add categories

--- React/React-shop/src/index.js
+++ React/React-shop/src/index.js
@@ -27,6 +27,7 @@ ReactDOM.render(
         <Router history={history}>
             <Route component={Layout}>
                 <Route path='/' component={Phones} />
+                <Route path='/categories/:id' component={Phones} />
             </Route>
             <Route path="phones/:id" component={Phone}></Route>
         </Router>
